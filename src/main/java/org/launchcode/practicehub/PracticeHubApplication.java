674ba20e458ee235a568f376f9fdf71936c89aff
Ultraviolet-N7 <meieramberj@gmail.com>
2021-04-10 17:24:30 -0500

    Added dependencies, created Hello, World! at index

---
+++ src/main/java/org/launchcode/practicehub/PracticeHubApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.practicehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/practicehub/PracticeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.practicehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
