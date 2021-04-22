0454cb4b3d5e7b181c1786175ecc9d4145952c85
Ultraviolet-N7 <meieramberj@gmail.com>
2021-04-21 22:55:25 -0500

    Initialized Practice model class

--- src/main/java/org/launchcode/practicehub/models/AbstractEntity.java
+++
@@ -1,5 +0,0 @@
-package org.launchcode.practicehub.models;
-
-
-public class AbstractEntity {
-}
--- src/main/java/org/launchcode/practicehub/models/Practice.java
+++ src/main/java/org/launchcode/practicehub/models/Practice.java
@@ -1,4 +1,35 @@
 package org.launchcode.practicehub.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Practice {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private int numCredits;
+
+    // This will be an enum type
+    // private PracticeType type;
+
+    // Date variable
+
+
+    public Practice(int numCredits) {
+        this.numCredits = numCredits;
+    }
+
+    public Practice(){}
+
+    public int getNumCredits() {
+        return numCredits;
+    }
+
+    public void setNumCredits(int numCredits) {
+        this.numCredits = numCredits;
+    }
 }
