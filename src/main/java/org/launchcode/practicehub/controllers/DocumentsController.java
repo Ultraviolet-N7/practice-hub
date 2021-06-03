663af251e001ffd95043323ef2e98128ebef6213
Ultraviolet-N7 <meieramberj@gmail.com>
2021-06-03 04:16:42 -0500

    Created Documents controller and documents template. Added documents link to navbar

--- src/main/java/org/launchcode/practicehub/controllers/DocumentsController.java
+++ src/main/java/org/launchcode/practicehub/controllers/DocumentsController.java
@@ -1,16 +1,16 @@
-//package org.launchcode.practicehub.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class DocumentsController {
-//
-//    @GetMapping
-//    public String displayDocumentsLinks() {
-//
-//        return "documents";
-//
-//    }
-//
-//}
+package org.launchcode.practicehub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DocumentsController {
+
+    @GetMapping("documents")
+    public String displayDocumentsLinks() {
+
+        return "documents";
+
+    }
+
+}
